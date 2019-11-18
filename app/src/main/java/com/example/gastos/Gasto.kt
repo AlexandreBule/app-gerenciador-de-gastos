304ef6f43f395a711b96d975c0a626cc6cffc05f
Victor Aguiar <vag2@cin.ufpe.br>
2019-11-18 20:56:39 -0300

    Create project base and SQLite configuration

---
+++ app/src/main/java/com/example/gastos/Gasto.kt
@@ -0,0 +1,12 @@
+package com.example.gastos
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="Gastos")
+data class Gasto(@PrimaryKey val description: String, val price: Double, val tag: String, val location: String, val day: String, val month: String, val year: String, val image: String) {
+
+    override fun toString(): String {
+        return description
+    }
+}
---
+++ app/src/main/java/com/example/gastos/GastoDAO.kt
@@ -0,0 +1,21 @@
+package com.example.gastos
+
+import androidx.room.*
+
+@Dao
+interface GastoDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun inserirGasto(vararg gasto:Gasto)
+    @Update
+    fun atualizarGasto(vararg gasto:Gasto)
+    @Delete
+    fun removerGasto(vararg gasto:Gasto)
+    @Query("SELECT * FROM Gastos")
+    fun todosGastos() : List<Gasto>
+    @Query("SELECT * FROM Gastos WHERE description LIKE :q")
+    fun buscaGastoPelaDescricao(q : String) : Gasto
+    @Query("SELECT * FROM Gastos WHERE month LIKE :q")
+    fun buscaGastoPeloMes(q : String) : Gasto
+    @Query("SELECT * FROM Gastos WHERE year LIKE :q")
+    fun buscaGastoPeloAno(q : String) : Gasto
+}
