d8f72fa4294c1b67abc670ffbbbd23b767ee2c8c
pamella <pammbsilva@gmail.com>
2019-12-08 17:26:20 -0300

    Add user device location to gasto

--- app/src/main/java/com/example/gastos/AddGastoActivity.kt
+++ app/src/main/java/com/example/gastos/AddGastoActivity.kt
@@ -1,9 +1,17 @@
 package com.example.gastos
 
+import android.app.PendingIntent.getActivity
 import android.content.Intent
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.os.Parcelable
+import android.provider.ContactsContract.Directory.PACKAGE_NAME
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.add_gasto.*
 import org.jetbrains.anko.doAsync
 import java.util.*
@@ -11,15 +19,56 @@ import java.util.*
 
 class AddGastoActivity : AppCompatActivity() {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_gasto)
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { local : Location? ->
+                Log.d("HERE", "HEERE")
+
+                val MyLat = local!!.getLatitude()
+                val MyLong = local!!.getLongitude()
+                Log.d("local", MyLat.toString())
+
+                val geocoder = Geocoder(this, Locale.getDefault())
+                var addresses: List<Address> = emptyList()
+                addresses = geocoder.getFromLocation(
+                    MyLat,
+                    MyLong,
+                    // In this sample, we get just a single address.
+                    1)
+
+                val cityName = addresses[0].getAddressLine(0)
+                val stateName = addresses[0].getAddressLine(1)
+                val countryName = addresses[0].getAddressLine(2)
+                Log.d("HEAAAAAAAAAAAAAAAAAARE", cityName)
+                location.setText(cityName)
+            }
 
         description.setOnFocusChangeListener {_, hasFocus ->
             val text = description.getText().toString()
             if (!hasFocus) {
                 tag.setText(getTag(text))
                 date.setText(getDate(text))
+
+            /*    val local = intent.getParcelableExtra("${PACKAGE_NAME}.LOCATION_DATA_EXTRA") as Parcelable
+                val geocoder = Geocoder(this, Locale.getDefault())
+                var addresses: List<Address> = emptyList()
+                addresses = geocoder.getFromLocation(
+                    local.latitude,
+                    local.longitude,
+                    // In this sample, we get just a single address.
+                    1)
+
+                val cityName = addresses[0].getAddressLine(0)
+                val stateName = addresses[0].getAddressLine(1)
+                val countryName = addresses[0].getAddressLine(2)*/
             }
         }
 
@@ -57,7 +106,7 @@ class AddGastoActivity : AppCompatActivity() {
 
     fun getTag(text: String) : String {
         val tag = text.substringAfter('#').split(' ')
-        if (tag.size > 0) {
+        if (tag.size > 1) {
             return tag[0]
         } else {
             return ""
@@ -66,8 +115,8 @@ class AddGastoActivity : AppCompatActivity() {
 
     fun getDate(text: String) : String {
         val c = Calendar.getInstance()
-        val today = c.get(Calendar.DATE).toString() + "/" + c.get(Calendar.MONTH).toString() +  "/" +  c.get(Calendar.YEAR).toString()
-        val yesterday = (c.get(Calendar.DATE) - 1).toString() + "/" + c.get(Calendar.MONTH).toString() +  "/" +  c.get(Calendar.YEAR).toString()
+        val today = c.get(Calendar.DATE).toString() + "/" + (c.get(Calendar.MONTH).toString() + 1) +  "/" +  c.get(Calendar.YEAR).toString()
+        val yesterday = (c.get(Calendar.DATE) - 1).toString() + "/" + (c.get(Calendar.MONTH).toString() + 1) +  "/" +  c.get(Calendar.YEAR).toString()
 
         if (text.contains("hoje")) {
             return today
