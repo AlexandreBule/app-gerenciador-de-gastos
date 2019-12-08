a7ee61ccd3604dd170106f83f6aa685644639e93
pamella <pammbsilva@gmail.com>
2019-12-08 09:47:22 -0300

    Update add gasto activity and style

--- app/src/main/java/com/example/gastos/AddGastoActivity.kt
+++ app/src/main/java/com/example/gastos/AddGastoActivity.kt
@@ -1,13 +1,8 @@
 package com.example.gastos
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.add_gasto.*
 
 class AddGastoActivity : AppCompatActivity() {
 
