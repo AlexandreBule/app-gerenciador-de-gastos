e043c5acc255b9c4bc065d73c5ef0dec06d15c36
pamella <pammbsilva@gmail.com>
2019-12-08 15:54:39 -0300

    Add function to get tag from text

--- app/src/main/java/com/example/gastos/AddGastoActivity.kt
+++ app/src/main/java/com/example/gastos/AddGastoActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.add_gasto.*
 import org.jetbrains.anko.doAsync
-
+import android.widget.Toast
 
 class AddGastoActivity : AppCompatActivity() {
 
@@ -14,13 +14,21 @@ class AddGastoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_gasto)
 
+        description.setOnFocusChangeListener {_, hasFocus ->
+            val text = description.getText().toString()
+            if (!hasFocus) {
+                val tagFromText = getTag(text)
+                tag.setText(tagFromText)
+            }
+        }
+
         btn_Add.setOnClickListener {
             // Get values from inputs
-            val description = description.getEditText()!!.getText().toString()
-            val price = if (price.getEditText()!!.getText().toString().isNullOrEmpty()) 0.0 else price.getEditText()!!.getText().toString().toDouble()
-            val tag = if (tag.getEditText()!!.getText().toString().isNullOrEmpty()) "" else tag.getEditText()!!.getText().toString()
-            val location = if (location.getEditText()!!.getText().toString().isNullOrEmpty()) "" else location.getEditText()!!.getText().toString()
-            val date = if (date.getEditText()!!.getText().toString().isNullOrEmpty()) listOf<String>("", "", "")  else date.getEditText()!!.getText().toString().split('/')
+            val description = description.getText().toString()
+            val price = if (price.getText().toString().isNullOrEmpty()) 0.0 else price.getText().toString().toDouble()
+            val tag = if (tag.getText().toString().isNullOrEmpty()) "" else tag.getText().toString()
+            val location = if (location.getText().toString().isNullOrEmpty()) "" else location.getText().toString()
+            val date = if (date.getText().toString().isNullOrEmpty()) listOf<String>("", "", "")  else date.getText().toString().split('/')
 
             val gasto = Gasto(
                 description = description,
@@ -43,6 +51,16 @@ class AddGastoActivity : AppCompatActivity() {
             // Go back to MainActivity
             val intent = Intent(applicationContext, MainActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun getTag(text: String): String {
+        val tag = text.substringAfter('#').split(' ')
+        print(text)
+        if (tag.size > 0) {
+            return tag[0]
+        } else {
+            return ""
         }
     }
 }
